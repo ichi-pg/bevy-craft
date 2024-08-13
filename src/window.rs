eb104267d7a9694d8aa4d9419cd48d46def44128
ichi-pg <digoogoo@gmail.com>
2024-08-13 20:28:01 +0900

    Scaling fullscreen

--- src/window.rs
+++ src/window.rs
@@ -1,25 +1,42 @@
 use crate::input::*;
 use bevy::prelude::*;
 use bevy::window::*;
+use bevy::winit::*;
 
-fn toggle_fullscreen(mut query: Query<&mut Window, With<PrimaryWindow>>, input: Res<Input>) {
+fn toggle_fullscreen(
+    mut query: Query<(Entity, &mut Window), With<PrimaryWindow>>,
+    winit_windows: NonSend<WinitWindows>,
+    input: Res<Input>,
+) {
     if !input.alt_pressed {
         return;
     }
     if !input.enter {
         return;
     }
-    for mut window in &mut query {
+    for (entity, mut window) in &mut query {
         match window.mode {
             WindowMode::Windowed => {
                 window.mode = WindowMode::BorderlessFullscreen;
+                match winit_windows.get_window(entity) {
+                    Some(winit_window) => match winit_window.current_monitor() {
+                        Some(monitor) => {
+                            let height = window.height();
+                            window
+                                .resolution
+                                .set_scale_factor(monitor.size().height as f32 / height);
+                        }
+                        None => todo!(),
+                    },
+                    None => todo!(),
+                }
             }
             _ => {
                 window.mode = WindowMode::Windowed;
+                window.resolution.set_scale_factor(1.0);
             }
         }
     }
-    // TODO scaling
 }
 
 pub struct WindowPlugin;
