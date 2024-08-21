376c2d01455489f771e01ea8cfb1cc2bd505ada3
ichi-pg <digoogoo@gmail.com>
2024-08-21 21:07:22 +0900

    Bugfix selected pickaxe

--- src/item_stats.rs
+++ src/item_stats.rs
@@ -4,6 +4,7 @@ use crate::item::*;
 use crate::item_node::*;
 use crate::item_selecting::*;
 use crate::player::*;
+use crate::ui_states::sync_changed;
 use bevy::prelude::*;
 use bevy_craft::*;
 
@@ -21,6 +22,9 @@ pub struct PickaxePower(pub f32);
 
 #[derive(Component, Stats)]
 pub struct MeleePower(pub f32);
+
+#[derive(Event, Default)]
+struct SelectedChanged;
 
 pub trait Stats {
     fn get(&self) -> f32;
@@ -71,9 +75,10 @@ fn sync_selected<T: Component + Stats>(
     Query<(&ItemID, &T), With<ItemStats>>,
     Query<&mut T, (With<PlayerController>, Without<ItemStats>)>,
     Res<SelectedItem>,
+    EventReader<SelectedChanged>,
 ) {
-    move |hotbar_query, stats_query, mut player_query, selected| {
-        if !selected.is_changed() {
+    move |hotbar_query, stats_query, mut player_query, selected, event_reader| {
+        if !selected.is_changed() && event_reader.is_empty() {
             return;
         }
         let mut value = init_value;
@@ -91,19 +96,20 @@ fn sync_selected<T: Component + Stats>(
             player_stats.set(value);
         }
     }
-    // FIXME when putted in or consumed
 }
 
 pub struct ItemStatsPlugin;
 
 impl Plugin for ItemStatsPlugin {
     fn build(&self, app: &mut App) {
+        app.add_event::<SelectedChanged>();
         app.add_systems(Startup, spawn_stats);
         app.add_systems(
             Update,
             (
                 sync_equipment::<MaxHealth>(100.0),
                 sync_selected::<PickaxePower>(100.0),
+                sync_changed::<HotbarItem, ItemID, SelectedChanged>,
             ),
         );
     }
